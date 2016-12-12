7707811f1a21e922c16e673434ed5fa0bbe9c41d
DESKTOP-O6Q4UNM\ADMIN <tahongtram@gmail.com>
2016-12-12 09:27:30 +0700

    commit3

--- application/frontend/views/template_menu_view.php
+++ application/frontend/views/template_menu_view.php
@@ -1,24 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$categories = $this->SHOP_Model->ShopAllCategories();
+$categories = $this->SHOP_Model->ShopCategoriesParent();
 
-$cha= array();
-$pt=0;
-for ($i=0; $i<sizeof($categories); $i++)
-{
-	if($categories[$i]['pid']=='0'){
-		$cha[$pt] = $categories[$i];
-		for ($j=0; $j<sizeof($categories); $j++)
-		{
-			if($categories[$j]['pid']==$categories[$i]['mva']){
-				$cha[$pt]['flag'] = true;
-			}else{$cha[$pt]['flag'] = false;}
-		}
-		$pt++;
-	}
-}
-
+//print_r($cha);
 $mod = $this->uri->segment(1);		
 if($mod=="" || $mod=='Welcome'){
 	$class='class="ui-menu-categories"';
@@ -29,23 +14,21 @@ if($mod=="" || $mod=='Welcome'){
 echo'
 	<ul '.$class.'>
 	<li>DANH MỤC SẢN PHẨM<i class="fa fa-bars"></i></li>';
-	for($c=0; $c < sizeof($cha); $c++)
+	for($c=0; $c < sizeof($categories); $c++)
 	{
 		echo'<li class="h">
-        <span><img src="'.base_url().'uploads/modules/categories/'.$cha[$c]['icon'].'"></span>
-        <a href="'.base_url().'index.php/shop/module/'.$cha[$c]['mva'].'">'.$cha[$c]['title'].'<i class="fa fa-angle-right hidden-xs"></i></a>';
+        <span><img src="'.base_url().'uploads/modules/categories/'.$categories[$c]['icon'].'"></span>
+        <a href="'.base_url().'index.php/shop/module/'.$categories[$c]['mva'].'">'.$categories[$c]['title'].'<i class="fa fa-angle-right hidden-xs"></i></a>';
+		$child = $this->SHOP_Model->ShopCategoriesChild($categories[$c]['mva']);
 		
-		if($cha[$c]['flag']==true)
+		if(sizeof($child)!=0)
 		{
 			echo'<ul>';
-			for($n=0; $n < sizeof($categories); $n++)
+			for($n=0; $n < sizeof($child); $n++)
 			{
+				echo'<li><span><img src="'.base_url().'uploads/modules/categories/'.$child[$n]['icon'].'"></span>
+			<a href="'.base_url().'index.php/shop/category/'.$child[$n]['mva'].'">'.$child[$n]['title'].'<i class="hidden-xs"></i></a></li>';
 				
-				if($categories[$n]['pid'] == $cha[$c]['mva']){ 
-					
-					echo'<li><span><img src="'.base_url().'uploads/modules/categories/'.$categories[$n]['icon'].'"></span>
-			<a href="'.base_url().'index.php/shop/category/'.$cha[$c]['mva'].'">'.$categories[$n]['title'].'<i class="hidden-xs"></i></a></li>';
-				}
 			}
 			echo'</ul>';
 		}
